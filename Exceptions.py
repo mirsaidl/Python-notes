9e414bd150085d4542a9314e1db9839a28795f6b
MirsaidPy <abdurasulovm149@gmail.com>
2023-10-20 22:05:38 +0900

    Handling

--- Exceptions.py
+++ Exceptions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def get_in(prompt):
     while True:     
         try:
@@ -11,29 +10,3 @@ def main():
     print(f"x is {x}")
 
 main()
-
-
-
-
-
-
-=======
-def get_in(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            pass
-    
-def main():
-    x = get_in("What's x? ")
-    print(f"x is {x}")
-
-main()
-
-
-
-
-
-
->>>>>>> 28945ad6dfd40fd5155079a8e96b4aa711615d98
