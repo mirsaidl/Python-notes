09776fdb988fbe8d7c69451cd5f8f12df814f7e6
MirsaidPy <abdurasulovm149@gmail.com>
2023-10-23 08:58:32 +0900

    Libraries part 2

---
+++ Cowsay.py
@@ -0,0 +1,10 @@
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.trex("Hello, " + sys.argv[1])
+
+#cowsay.char_names
+cowsay_names = ['beavis', 'cheese', 'cow', 'daemon', 'dragon', 'fox', 'ghostbusters', 'kitty',
+'meow', 'miki', 'milk', 'octopus', 'pig', 'stegosaurus', 'stimpy', 'trex', 
+'turkey', 'turtle', 'tux']
